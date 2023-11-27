5cf85ed841bc3ad89b4c67c95141fce86123eb71
Michle99 <sammiyonyx@gmail.com>
2023-11-26 23:49:42 -0500

    Updated HelloControllerTest to controller package

--- src/test/java/com/micronaut/controller/HelloControllerTest.java
+++ src/test/java/com/micronaut/controller/HelloControllerTest.java
@@ -1,4 +1,4 @@
-package com.micronaut;
+package com.micronaut.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
